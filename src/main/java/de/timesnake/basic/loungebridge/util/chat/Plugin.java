755a8a0d0d7f1798ba8bb1e6fceccca8b7418a4a
funzter <funzter@mail.timesnake.de>
2022-11-11 20:31:15 +0100

    add logger to plugins, add log command

--- src/main/java/de/timesnake/basic/loungebridge/util/chat/Plugin.java
+++ src/main/java/de/timesnake/basic/loungebridge/util/chat/Plugin.java
@@ -1,5 +1,5 @@
 /*
- * basic-lounge-bridge.main
+ * timesnake.basic-lounge-bridge.main
  * Copyright (C) 2022 timesnake
  *
  * This program is free software; you can redistribute it and/or
@@ -18,12 +18,17 @@
 
 package de.timesnake.basic.loungebridge.util.chat;
 
+import de.timesnake.library.basic.util.LogHelper;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class Plugin extends de.timesnake.basic.bukkit.util.chat.Plugin {
 
-    public static final Plugin LOUNGE = new Plugin("Lounge", "BLB");
-    public static final Plugin GAME = new Plugin("Game", "BLG");
+    public static final Plugin LOUNGE = new Plugin("Lounge", "BLB", LogHelper.getLogger("Lounge", Level.INFO));
+    public static final Plugin GAME = new Plugin("Game", "BLG", LogHelper.getLogger("Game", Level.INFO));
 
-    protected Plugin(String name, String code) {
-        super(name, code);
+    protected Plugin(String name, String code, Logger logger) {
+        super(name, code, logger);
     }
 }
